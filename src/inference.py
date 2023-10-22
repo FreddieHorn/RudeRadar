9691d196d729722b1671f2ea2bedf3c2514ad283
Fryderyk Róg <67193178+FreddieHorn@users.noreply.github.com>
2023-10-22 22:02:01 +0200

    added inference endpoint

---
+++ src/inference.py
@@ -0,0 +1,39 @@
+from distilBERT import Regressor
+from data_processing import DataPreprocessor
+
+from transformers import AutoTokenizer, DistilBertModel
+import torch
+
+class RudenessDeterminator:
+    def __init__(self) -> None:
+        model_name = 'distilbert-base-uncased'
+        checkpoint_path = "checkpoints/DistilBERT-NAT-NP-WITH_SCHEDULER_RUN2_NO_FREEZE-epoch=19-val_loss=0.03.ckpt"
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        distilbert = DistilBertModel.from_pretrained(model_name, num_labels = 1)
+
+        self.processor = DataPreprocessor()
+        self.model = Regressor.load_from_checkpoint(checkpoint_path=checkpoint_path, map_location=torch.device(device), bertlike_model = distilbert)
+        self.model.to(device)
+        self.model.eval()
+
+    def measure_rudeness(self, text):
+        cleaned_text = self.processor.processBERT(text)
+        encoded_text = self.tokenizer(cleaned_text,
+                        add_special_tokens=True,
+                        padding="max_length",
+                        truncation=True,
+                        max_length=200,
+                        return_attention_mask=True,
+                        return_tensors="pt")
+        
+        input_ids = encoded_text['input_ids']
+        attention_mask = encoded_text['attention_mask']
+
+        with torch.no_grad():
+            prediction = self.model(input_ids, attention_mask)
+
+        
+
+        return prediction.item()
