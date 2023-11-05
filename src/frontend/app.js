568c2ff4e9c122d41e51aadf9441c91d66be9d0f
FreddieHorn <fryderykrog7@gmail.com>
2023-11-05 14:49:55 +0100

    added navbar and records button

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,11 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    const homeButton = document.getElementById("homeButton");
+    const recordsButton = document.getElementById("recordsButton");
+    const homeContent = document.getElementById("homeContent");
+    const recordsContent = document.getElementById("recordsContent");
+    
+    homeButton.addEventListener("click", () => {
+        homeContent.style.display = "block";
+        recordsContent.style.display = "none";
+
+        homeButton.classList.add("active");
+        recordsButton.classList.remove("active");
+    });
+
+    recordsButton.addEventListener("click", () => {
+        homeContent.style.display = "none";
+        recordsContent.style.display = "block";
+
+        recordsButton.classList.add("active");
+        homeButton.classList.remove("active");
+    });
+
     const inputText = document.getElementById("inputText");
     const analyzeButton = document.getElementById("analyzeButton");
     const scoreElement = document.getElementById("score");
+    const loadingBar = document.getElementById("loadingBar");
 
     analyzeButton.addEventListener("click", () => {
         const text = inputText.value;
 
+        loadingBar.style.width = "100%";
         // Simulate sending the text to the backend for analysis.
         // Replace this with your actual backend API call.
         //const dummyScore = Math.random() * 2 - 1; // Generate a random score between -1 and 1.
@@ -33,6 +57,10 @@ document.addEventListener("DOMContentLoaded", function () {
             })
             .catch((error) => {
                 console.error("API call error:", error);
+            })
+            .finally(() => {
+                // Hide the loading bar
+                loadingBar.style.width = "0";
             });
     });
 
